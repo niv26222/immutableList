da6c288f4f28fa7be615ec60b1e8a8aedbaa1d26
Niv Ben Abat <niv.benavat@gmail.com>
2019-03-03 10:10:43 +0200

    Add files via upload

---
+++ immutableList.java
@@ -0,0 +1,24 @@
+import com.google.common.collect.ImmutableList; 
+import java.util.*; 
+  
+class GFG { 
+  
+    // Function to create ImmutableList from List 
+    public static <T> void iList(List<T> list) 
+    { 
+        // Create ImmutableMap from Map using copyOf() 
+        ImmutableList<T> immutableList = 
+                          ImmutableList.copyOf(list); 
+  
+        // Print the ImmutableMap 
+        System.out.println(immutableList); 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        List<String> list = new ArrayList<>( 
+            Arrays.asList("Geeks", "For", "Geeks")); 
+  
+        iList(list); 
+    } 
+} 
